e2795ae20d273d9de0a1334ff40d1deb453ff593
premod_1 <premod@pramixit.com>
2025-06-15 00:38:28 +0530

    Add gallery section to Contact Us page with image display and hover effects

--- resources/views/frontend/pages/new_contact.blade.php
+++ resources/views/frontend/pages/new_contact.blade.php
@@ -41,5 +41,86 @@
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.8599026831743!2d80.35615!3d7.033359!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae30872c88fb6cb%3A0xda2a366134eb5de4!2sKegalle%2C%20Sri%20Lanka!5e0!3m2!1sen!2sus!4v1718028789529!5m2!1sen!2sus"
             width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
+               <!-- Gallery Section -->
+        <section class="pb-4 bg-white" id="gallery">
+            {{-- <div class="w-full flex pt-10 pb-5 flex-col justify-center items-center gap-5">
+                <div class="text-xl sm:text-3xl flex justify-center font-sec">Capture the Memories</div>
+                <div class="text-3xl sm:text-5xl font-black flex justify-center font-pri">Gallery</div>
+                <div class="sm:w-3/7 flex justify-center text-center font-pri text-sm sm:text-md">Explore the beauty of Sri Lanka through our travelers' experiences</div>
+            </div> --}}
+
+            <div class="w-full flex-wrap sm:flex-nowrap flex">
+                <div class="h-auto max-w-1/2 sm:max-w-1/7 sm:w-1/7 grow p-[1px] sm:p-1 group cursor-pointer">
+                    <div class="relative overflow-hidden">
+                        <img src="{{ asset('new_frontend/Assets/img(10).png') }}" class="w-full h-auto transition-all duration-500 transform group-hover:scale-110" alt="Beach view">
+                        <div class="w-full opacity-0 group-hover:opacity-100 duration-300 h-full top-0 absolute flex justify-center items-center p-5">
+                            <div class="w-full h-full absolute flex justify-center items-center bg-black/50">
+                                <img src="{{ asset('new_frontend/Assets/Group 23469.png') }}" alt="View image">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="h-auto max-w-1/2 sm:max-w-1/7 sm:w-1/7 grow p-[1px] sm:p-1 group cursor-pointer">
+                    <div class="relative overflow-hidden">
+                        <img src="{{ asset('new_frontend/Assets/img(11).png') }}" class="w-full h-auto transition-all duration-500 transform group-hover:scale-110" alt="Mountain view">
+                        <div class="w-full opacity-0 group-hover:opacity-100 duration-300 h-full top-0 absolute flex justify-center items-center p-5">
+                            <div class="w-full h-full absolute flex justify-center items-center bg-black/50">
+                                <img src="{{ asset('new_frontend/Assets/Group 23469.png') }}" alt="View image">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="h-auto max-w-1/2 sm:max-w-1/7 sm:w-1/7 grow p-[1px] sm:p-1 group cursor-pointer">
+                    <div class="relative overflow-hidden">
+                        <img src="{{ asset('new_frontend/Assets/img(12).png') }}" class="w-full h-auto transition-all duration-500 transform group-hover:scale-110" alt="Temple view">
+                        <div class="w-full opacity-0 group-hover:opacity-100 duration-300 h-full top-0 absolute flex justify-center items-center p-5">
+                            <div class="w-full h-full absolute flex justify-center items-center bg-black/50">
+                                <img src="{{ asset('new_frontend/Assets/Group 23469.png') }}" alt="View image">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="h-auto max-w-1/2 sm:max-w-1/7 sm:w-1/7 grow p-[1px] sm:p-1 group cursor-pointer">
+                    <div class="relative overflow-hidden">
+                        <img src="{{ asset('new_frontend/Assets/img(13).png') }}" class="w-full h-auto transition-all duration-500 transform group-hover:scale-110" alt="Wildlife view">
+                        <div class="w-full opacity-0 group-hover:opacity-100 duration-300 h-full top-0 absolute flex justify-center items-center p-5">
+                            <div class="w-full h-full absolute flex justify-center items-center bg-black/50">
+                                <img src="{{ asset('new_frontend/Assets/Group 23469.png') }}" alt="View image">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="h-auto max-w-1/2 sm:max-w-1/7 sm:w-1/7 grow p-[1px] sm:p-1 group cursor-pointer">
+                    <div class="relative overflow-hidden">
+                        <img src="{{ asset('new_frontend/Assets/img(14).png') }}" class="w-full h-auto transition-all duration-500 transform group-hover:scale-110" alt="Cultural view">
+                        <div class="w-full opacity-0 group-hover:opacity-100 duration-300 h-full top-0 absolute flex justify-center items-center p-5">
+                            <div class="w-full h-full absolute flex justify-center items-center bg-black/50">
+                                <img src="{{ asset('new_frontend/Assets/Group 23469.png') }}" alt="View image">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="h-auto max-w-1/2 sm:max-w-1/7 sm:w-1/7 grow p-[1px] sm:p-1 group cursor-pointer">
+                    <div class="relative overflow-hidden">
+                        <img src="{{ asset('new_frontend/Assets/img(15).png') }}" class="w-full h-auto transition-all duration-500 transform group-hover:scale-110" alt="Resort view">
+                        <div class="w-full opacity-0 group-hover:opacity-100 duration-300 h-full top-0 absolute flex justify-center items-center p-5">
+                            <div class="w-full h-full absolute flex justify-center items-center bg-black/50">
+                                <img src="{{ asset('new_frontend/Assets/Group 23469.png') }}" alt="View image">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="h-auto max-w-1/2 sm:max-w-1/7 sm:w-1/7 grow p-[1px] sm:p-1 group cursor-pointer">
+                    <div class="relative overflow-hidden">
+                        <img src="{{ asset('new_frontend/Assets/img(15).png') }}" class="w-full h-auto transition-all duration-500 transform group-hover:scale-110" alt="Resort view">
+                        <div class="w-full opacity-0 group-hover:opacity-100 duration-300 h-full top-0 absolute flex justify-center items-center p-5">
+                            <div class="w-full h-full absolute flex justify-center items-center bg-black/50">
+                                <img src="{{ asset('new_frontend/Assets/Group 23469.png') }}" alt="View image">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 @endsection
